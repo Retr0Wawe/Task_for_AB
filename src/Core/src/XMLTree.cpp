931208c0a1b0ad86be88792f8a1ca4aaf4dcc9ed
Retr0Wawe <kashelot.petrovich@mail.ru>
2022-02-14 03:12:40 +0300

    Add files via upload

--- src/Core/src/XMLTree.cpp
+++ src/Core/src/XMLTree.cpp
@@ -58,6 +58,7 @@ void XMLtree::AddElement(const char ch) noexcept
 			pEmployment->InsertEndChild(pFirstElem);
 		}
 	}
+
 	else if (ch == 'e') {
 		XMLElement* pElem = pTop->FirstChildElement("department");
 		std::cout << "Enter department name: ";
@@ -67,19 +68,44 @@ void XMLtree::AddElement(const char ch) noexcept
 			auto str_atr = pElem->Attribute("name");
 
 			if (str == str_atr) {
-				std::cout << pElem->Attribute("name");
 				break;
 			}
 			pElem = pElem->NextSiblingElement();
 		}
-	}
 
-	doc.SaveFile(file_path);
+		if(pElem != nullptr){
+			XMLElement* pEmployments = pElem->FirstChildElement("employments");
+			
+			if(pEmployments != nullptr){
+				XMLElement* pEmployment = pEmployments->FirstChildElement("employment");
+				str.clear();
+
+				std::cout << "Enter element name: ";
+				std::getline(std::cin, str);
+
+				elem_names.push_back(str);		
+				XMLElement* pNewElem = doc.NewElement(str.c_str());
+				std::cout << "Enter data for element '" << str << "': ";
+				str.clear();
+				std::getline(std::cin, str);
+				pNewElem->SetText(str.c_str());
+				pEmployment->InsertEndChild(pNewElem);
+			} else{
+				std::cout << "Employment not recognized!" << std::endl;
+			}
+		} else{
+			std::cout << "Department not found!" << std::endl;
+		}
+	}
+	//переделать можно по разному, принцип добавления элемента остается тем же.
+	//на windows сущестувет проблема с кодировкой, из за которой не получается спарсить данные в нужной кодировке.
+	//но даже если решить эту проблему, все равно ничего не заработает из-за того что нативные методы библиотеки(tinyxml2)
+	//не поддерживают const wchar_t*, потому что нет перегрузки функции GetText для этого типа данных.
 }
 
 void XMLtree::ChangeElement() noexcept
 {
-
+	
 }
 
 void XMLtree::DeleteElement() noexcept
@@ -105,14 +131,12 @@ void XMLtree::PrintTree() const noexcept
 					pEmployment = pEmployment->NextSiblingElement("employment");
 					std::cout << std::endl;
 				}
-			}
-			else {
+			} else {
 				std::cout << "Employments not recognized!" << std::endl;
 			}
 			pDepartaments = pDepartaments->NextSiblingElement("department");
 		}
-	}
-	else {
+	} else {
 		std::cout << "File not recognized!" << std::endl;
 	}
 }
